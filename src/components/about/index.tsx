1ef33bf03fb531d0f4177c6cb38bbfc28dec6c41
Ivan Hribernik <cihribernik@gmail.com>
2025-01-16 20:56:24 -0300

    added new photo

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import hero from "../../assets/images/hero.jpg";
 type Props = {
   puesto: string;
   paragrap: string[];
@@ -11,10 +11,22 @@ const About = ({ puesto, paragrap }: Props) => {
       <section id="about" className="py-16 bg-white">
         <div className="container mx-auto text-center">
           <h2 className="text-3xl font-bold mb-4">Sobre mí</h2>
-          <p className="text-lg text-gray-600 mb-8">{puesto}</p>
-          {paragrap.map((p) => (
-            <p className="text-gray-700">{p}</p>
-          ))}
+          <div className="container flex">
+            <div className="relative w-40 h-50 mx-auto mb-4">
+              <img
+                src={hero}
+                alt="Foto de perfil"
+                className="w-full h-full rounded-full object-cover shadow-lg border-4 border-yellow-500"
+              />
+            </div>
+
+            <div className="text-left">
+              <p className="text-lg text-gray-600 mb-8">{puesto}</p>
+              {paragrap.map((p) => (
+                <p className="text-gray-700">{p}</p>
+              ))}
+            </div>
+          </div>
         </div>
       </section>
     </>
